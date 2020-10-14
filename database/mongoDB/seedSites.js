f4546603a36fd86eff43bf1834bda8f84c43e72b
Paul Choi <paulsoonminchoi@gmail.com>
2020-10-14 15:33:30 -0700

    created one successful mongoDB change to work with sites

---
+++ database/mongoDB/seedSites.js
@@ -0,0 +1,48 @@
+const faker = require('faker');
+const db = require('./index.js');
+
+const weather = ['Cloudy', 'Clear', 'Partly Cloudy', 'Raining', 'Snowing', 'Drizzling'];
+
+const createSite = () => {
+  const site = {};
+  const randomIndex = Math.floor(Math.random() * weather.length);
+
+  site.siteName = faker.address.streetName();
+
+  site.siteArea = faker.address.city();
+
+  site.siteState = faker.address.state();
+
+  site.elevation = faker.random.number(5300);
+
+  site.temperature = faker.random.number(120);
+
+  site.weather = weather[randomIndex];
+
+  site.distance = faker.random.float({
+    min: 0.1,
+    max: 100.2,
+    precision: 0.1,
+  });
+
+  return site;
+};
+
+const createSites = () => {
+  const sitesArr = [];
+
+  for (let i = 0; i < 100; i++) {
+    sitesArr.push(createSite());
+  }
+
+  db.Sites.insertMany(sitesArr, (err, results) => {
+    if (err) {
+      console.error(err);
+    } else {
+      db.connection.close();
+      console.log('Seed successful!');
+    }
+  });
+};
+
+createSites();
