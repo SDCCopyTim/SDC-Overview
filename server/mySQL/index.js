b5c6457311d2dde37c04dd7dc66f7fd346aab680
Paul Choi <paulsoonminchoi@gmail.com>
2020-10-22 16:33:43 -0700

    Added newrelic and K6 stress test script

--- server/mySQL/index.js
+++ server/mySQL/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyparser = require('body-parser');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
 const router = require('./router.js');
@@ -9,7 +9,7 @@ const port = 3003;
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../../client/dist')));
